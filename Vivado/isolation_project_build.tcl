25387209e52467b621f8ca4d3077f5c22767b9d8
Stephen Donchez <sdonchez@villanova.edu>
2022-05-06 13:21:50 -0400

    Fix constraints set

--- Vivado/isolation_project_build.tcl
+++ Vivado/isolation_project_build.tcl
@@ -181,8 +181,13 @@ if {[string equal [get_filesets -quiet constrs_1] ""]} {
 # Set 'constrs_1' fileset object
 set obj [get_filesets constrs_1]
 
-# Set 'constrs_1' fileset properties
-set obj [get_filesets constrs_1]
+# Add/Import constrs file and set constrs file properties
+set file "[file normalize "$origin_dir/Constraints.xdc"]"
+set file_added [add_files -norecurse -fileset $obj [list $file]]
+set file "$origin_dir/Constraints.xdc"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
+set_property -name "file_type" -value "XDC" -objects $file_obj
 # Create 'sim_1' fileset (if not found)
 if {[string equal [get_filesets -quiet sim_1] ""]} {
   create_fileset -simset sim_1
